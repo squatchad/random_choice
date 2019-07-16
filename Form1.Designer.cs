7e579f884a36367d9baf93ce3b6701b93261ea87
DESKTOP-CRCACA1\squatch-pc <junkmail072@comcast.net>
2019-07-16 06:29:07 -0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace random_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.randomButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox.Location = new System.Drawing.Point(8, 3);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(389, 296);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            // 
+            // randomButton
+            // 
+            this.randomButton.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.randomButton.Location = new System.Drawing.Point(13, 306);
+            this.randomButton.Name = "randomButton";
+            this.randomButton.Size = new System.Drawing.Size(75, 38);
+            this.randomButton.TabIndex = 1;
+            this.randomButton.Text = "random";
+            this.randomButton.UseVisualStyleBackColor = true;
+            this.randomButton.Click += new System.EventHandler(this.random_button_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(104, 312);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(45, 18);
+            this.resultLabel.TabIndex = 2;
+            this.resultLabel.Text = "result";
+            this.resultLabel.Click += new System.EventHandler(this.resultLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 356);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.randomButton);
+            this.Controls.Add(this.richTextBox);
+            this.Name = "Form1";
+            this.Text = "Random Choice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button randomButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace random_game
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            resultLabel.Text = "";
+        }
+
+        private void random_button_Click(object sender, EventArgs e)
+        {
+            int lineCount = richTextBox.Lines.Count();
+            if (lineCount > 0)
+            {
+                if (richTextBox.Lines[lineCount - 1] == "")
+                {
+                    lineCount--;
+                }
+                int randomLine = random.Next(0, lineCount);
+                String value = richTextBox.Lines[randomLine];
+                resultLabel.Text = value;
+            }
+        }
+
+        private void resultLabel_Click(object sender, EventArgs e)
+        {
+            resultLabel.Text = "";
+        }
+    }
+}
